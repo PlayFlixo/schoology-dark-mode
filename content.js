7d7c28d477837a6e11af036e4dee15b803287e81
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-07-21 20:49:18 -0500

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-// content.js
-document.addEventListener('DOMContentLoaded', () => {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
-  link.href = chrome.runtime.getURL('styles.css');
-  document.head.appendChild(link);
-});
